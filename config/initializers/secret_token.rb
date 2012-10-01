47ffec4fcbf129317f1c2ec871824b559f4c7b57
abonec <abonec@gmail.com>
2012-10-01 10:28:24 +0400

    @= generated default rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Faceb00k::Application.config.secret_token = 'f99104db7800b388c022f1da34dd98a553c191ce1d69509b3d5a110377b9d3f2d8648d85955d15e52848243d059c36f646ea0d297c912ec5f6bc3c9e3b93251f'
